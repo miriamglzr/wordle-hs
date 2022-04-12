8c6db3af2da351e932552d21ed20482f6b6005a5
miriam <miriam_glzr@hotmail.com>
2022-04-12 23:32:43 +0200

    tried 5 attempts working with bugs for length

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import {useState} from 'react';
 import './App.css';
 import WordColumns from './WordColumns';
 
 function App () {
+  const [attempts, setAttempts] = useState (0);
+  const [wordList, setWordList] = useState ([
+    {word: '', blocked: false},
+    {word: '', blocked: true},
+    {word: '', blocked: true},
+    {word: '', blocked: true},
+    {word: '', blocked: true},
+  ]);
+
   return (
     <div className="App">
 
       <header className="App-header">
-        <h1 className="mt-3"> Food Wordle</h1>
+        <h1 className="mt-3">Food Wordle</h1>
         <div className="container mt-5 word-list">
-          <WordColumns />
+          {wordList.map ((attempt, index) => (
+            <WordColumns
+              setAttempts={setAttempts}
+              attempts={attempts}
+              wordList={wordList}
+              setWordList={setWordList}
+              key={index}
+              attempt={attempt}
+              attemptIndex={index}
+            />
+          ))}
 
         </div>
       </header>
--- src/WordColumns.js
+++ src/WordColumns.js
@@ -2,7 +2,14 @@ import React, {useState} from 'react';
 import useKeypress from './useKeypress';
 import './index.css';
 
-export default function WordColumns () {
+export default function WordColumns({
+  attempts,
+  setAttempts,
+  wordList,
+  setWordList,
+  attempt,
+  attemptIndex,
+}) {
   const pizza = ['p', 'i', 'z', 'z', 'a'];
   const [wordle, setWordle] = useState ([
     {letter: '', style: 'none'},
@@ -12,38 +19,60 @@ export default function WordColumns () {
     {letter: '', style: 'none'},
   ]);
   const [length, setlength] = useState (0);
-  //const [text, setText] = useState ('');
+  const [blockedWord, setBlockedWord] = useState (false);
 
-  useKeypress (key => {
-    if (key === 'Backspace') {
-      const newWordle = wordle;
-      newWordle.splice (length - 1, 1, {letter: '', style: 'none'});
-      setWordle (newWordle);
-      length !== 0 && setlength (length - 1);
-    } else if (length === 5 && key === 'Enter') {
-      submitWord ();
-      // alert (pizza);
-      /// check if correct word
-    } else if (length === 5 && key.length === 1 && /[a-zA-Z]/.test (key)) {
-      alert ('5 letters max');
-    } else if (/[a-zA-Z]/.test (key) && key.length === 1) {
-      let blank = wordle.map (object => object.letter).indexOf ('');
-      console.log (blank);
-      let newWord = wordle;
-      newWord.splice (blank, 1, {letter: key.toLowerCase (), style: 'none'});
-      setWordle (newWord);
-      setlength (length + 1);
+  useKeypress (async key => {
+    console.log (wordList[1].blocked);
+    if (!attempt.blocked) {
+      if (key === 'Backspace') {
+        const newWordle = wordle;
+        newWordle.splice (length - 1, 1, {letter: '', style: 'none'});
+        setWordle (newWordle);
+        length !== 0 && setlength (length - 1);
+      } else if (length === 5 && key === 'Enter') {
+        await submitWord ();
+        await setBlockedWord (true);
+        await setAttempts (attempts + 1);
+
+        let newWordle = [];
+        wordle.map (letter => {
+          newWordle.push (letter.letter);
+        });
+        newWordle = newWordle.join ('');
+        let wordAttempt = {word: newWordle, blocked: true};
+        let nextWordAttempt = {word: '', blocked: false};
+        const newWordList = wordList;
+        wordList.splice (attemptIndex, 1, wordAttempt);
+        wordList.splice (attemptIndex + 1, 1, nextWordAttempt);
+        await setWordList (newWordList);
+        await setBlockedWord (true);
+        await setlength (0);
+
+        // alert (pizza);
+        /// check if correct word
+      } else if (length === 5 && key.length === 1 && /[a-zA-Z]/.test (key)) {
+        alert ('5 letters max');
+      } else if (/[a-zA-Z]/.test (key) && key.length === 1) {
+        let blank = wordle.map (object => object.letter).indexOf ('');
+        // console.log (blank);
+        let newWord = wordle;
+        newWord.splice (blank, 1, {letter: key.toLowerCase (), style: 'none'});
+        setWordle (newWord);
+        setlength (length + 1);
+      }
     }
   });
 
   const submitWord = () => {
     let newWord = wordle;
     wordle.map ((attemptLetter, i) => {
+      //set background color gray for wrong letters as default
       attemptLetter.style = 'wrong-letter';
       pizza.map ((correctLetter, j) => {
-        console.log (correctLetter);
+        //console.log (correctLetter);
+        //if correct letter found change background color
         if (attemptLetter.letter === correctLetter) {
-          console.log ('same letter');
+          //  console.log ('same letter');
           let letterIndex = newWord
             .map (object => object.letter)
             .indexOf (attemptLetter.letter);
